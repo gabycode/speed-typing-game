c214685e010f8418fd921e9c9d69f04b71b5649a
gabycode <86427446+gabycode@users.noreply.github.com>
2023-03-17 16:24:27 -0400

    add:function to console value in textarea

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import "../App.css";
 
 const Textarea = () => {
-  return <textarea />;
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  console.log(text);
+  return <textarea value={text} onChange={handleChange} />;
 };
 
 export default Textarea;
