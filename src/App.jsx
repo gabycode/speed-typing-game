6c4b735a8224336da024f2b7d4627123b3c74f88
gabycode <86427446+gabycode@users.noreply.github.com>
2023-03-20 15:45:18 -0400

    add: states and function

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,25 @@ import "./App.css";
 import Textarea from "./components/Textarea";
 
 function App() {
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setText(value);
+  };
+
+  function countWords(text) {
+    const wordsArr = text.trim().split(" ");
+    console.log(wordsArr);
+    return wordsArr.length;
+  }
+
   return (
     <div className="App">
       <h1 className="title">How fast do you type?</h1>
-      <Textarea />
+      <Textarea text={text} handleChange={handleChange} />
       <h4>Time remaining: ???</h4>
-      <button>Start</button>
+      <button onClick={() => countWords(text)}>Start</button>
       <h1>Word count: ???</h1>
     </div>
   );
