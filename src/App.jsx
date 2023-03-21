4e117a49778eedc77fd8f5815a32e5cd4f404bc8
gabycode <86427446+gabycode@users.noreply.github.com>
2023-03-21 12:13:46 -0400

    add: functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Textarea from "./components/Textarea";
 
 function App() {
   const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(2);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (timeRemaining === 0) {
+      setIsTimeRunning(false);
+    }
+  }, [timeRemaining, isTimeRunning]);
 
   const handleChange = (e) => {
     const { value } = e.target;
@@ -12,16 +24,23 @@ function App() {
 
   function countWords(text) {
     const wordsArr = text.trim().split(" ");
-    console.log(wordsArr);
-    return wordsArr.length;
+    return wordsArr.filter((word) => word !== "").length;
+  }
+
+  function handleClick() {
+    if (timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+    }
   }
 
   return (
     <div className="App">
       <h1 className="title">How fast do you type?</h1>
       <Textarea text={text} handleChange={handleChange} />
-      <h4>Time remaining: ???</h4>
-      <button onClick={() => countWords(text)}>Start</button>
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={() => setIsTimeRunning(true)}>Start</button>
       <h1>Word count: ???</h1>
     </div>
   );
