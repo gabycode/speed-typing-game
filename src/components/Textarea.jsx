e60b22a308ce87a74d60f581c678e3a195c0cf52
gabycode <86427446+gabycode@users.noreply.github.com>
2023-03-21 12:38:40 -0400

    add: functionality

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -2,7 +2,13 @@ import { useState } from "react";
 import "../App.css";
 
 const Textarea = (props) => {
-  return <textarea value={props.text} onChange={props.handleChange} />;
+  return (
+    <textarea
+      value={props.text}
+      onChange={props.handleChange}
+      disabled={!props.isTimeRunning}
+    />
+  );
 };
 
 export default Textarea;
