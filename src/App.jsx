02639fd29bdb2536c8a6ce33f92200d0024b1edd
gabycode <86427446+gabycode@users.noreply.github.com>
2023-03-21 12:38:23 -0400

    add: functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import "./App.css";
 import Textarea from "./components/Textarea";
 
 function App() {
+  const STARTING_TIME = 5;
+
   const [text, setText] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(2);
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
   const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
 
   useEffect(() => {
     if (isTimeRunning && timeRemaining > 0) {
@@ -13,9 +16,20 @@ function App() {
         setTimeRemaining((prevTime) => prevTime - 1);
       }, 1000);
     } else if (timeRemaining === 0) {
-      setIsTimeRunning(false);
+      endGame();
     }
   }, [timeRemaining, isTimeRunning]);
+
+  function startGame() {
+    setIsTimeRunning(true);
+    setTimeRemaining(STARTING_TIME);
+    setText("");
+  }
+
+  function endGame() {
+    setIsTimeRunning(false);
+    setWordCount(countWords(text));
+  }
 
   const handleChange = (e) => {
     const { value } = e.target;
@@ -27,21 +41,19 @@ function App() {
     return wordsArr.filter((word) => word !== "").length;
   }
 
-  function handleClick() {
-    if (timeRemaining > 0) {
-      setTimeout(() => {
-        setTimeRemaining((prevTime) => prevTime - 1);
-      }, 1000);
-    }
-  }
-
   return (
     <div className="App">
       <h1 className="title">How fast do you type?</h1>
-      <Textarea text={text} handleChange={handleChange} />
+      <Textarea
+        text={text}
+        handleChange={handleChange}
+        isTimeRunning={isTimeRunning}
+      />
       <h4>Time remaining: {timeRemaining}</h4>
-      <button onClick={() => setIsTimeRunning(true)}>Start</button>
-      <h1>Word count: ???</h1>
+      <button onClick={startGame} disabled={isTimeRunning}>
+        Start
+      </button>
+      <h1>Word count: {wordCount}</h1>
     </div>
   );
 }
