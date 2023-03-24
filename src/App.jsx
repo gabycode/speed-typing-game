c8b0ae8956464d44984e3af865215b709339924a
gabycode <86427446+gabycode@users.noreply.github.com>
2023-03-24 11:38:15 -0400

    add: custom hook useLogic()

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,20 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Textarea from "./components/Textarea";
+import useLogic from "./components/useLogic";
 
 function App() {
-  const STARTING_TIME = 15;
-
-  const [text, setText] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
-  const [isTimeRunning, setIsTimeRunning] = useState(false);
-  const [wordCount, setWordCount] = useState(0);
-  const textAreaRef = useRef(null);
-
-  useEffect(() => {
-    if (isTimeRunning && timeRemaining > 0) {
-      setTimeout(() => {
-        setTimeRemaining((prevTime) => prevTime - 1);
-      }, 1000);
-    } else if (timeRemaining === 0) {
-      endGame();
-    }
-  }, [timeRemaining, isTimeRunning]);
-
-  function startGame() {
-    setIsTimeRunning(true);
-    setTimeRemaining(STARTING_TIME);
-    setText("");
-    textAreaRef.current.disabled = false;
-    textAreaRef.current.focus();
-  }
-
-  function endGame() {
-    setIsTimeRunning(false);
-    setWordCount(countWords(text));
-  }
-
-  const handleChange = (e) => {
-    const { value } = e.target;
-    setText(value);
-  };
-
-  function countWords(text) {
-    const wordsArr = text.trim().split(" ");
-    return wordsArr.filter((word) => word !== "").length;
-  }
+  const {
+    text,
+    timeRemaining,
+    setTimeRemaining,
+    isTimeRunning,
+    handleChange,
+    textAreaRef,
+    startGame,
+    endGame,
+    wordCount,
+  } = useLogic();
 
   return (
     <div className="App">
